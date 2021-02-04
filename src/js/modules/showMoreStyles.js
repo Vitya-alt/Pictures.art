99ee5cbcbcb2a5da8ee3fdf7c0834e09bde5212e
Vitya-alt <oxov_2000@mail.ru>
2021-02-04 11:26:20 +0300

    added cards in db and get its

--- src/js/modules/showMoreStyles.js
+++ src/js/modules/showMoreStyles.js
@@ -1,7 +1,10 @@
-const showMoreStyles = (trigger, style) => {
-    const cards = document.querySelectorAll(style),
-          btn = document.querySelector(trigger);
+import {getResource} from "../services/requests";
 
+const showMoreStyles = (trigger, wrapper) => {
+    const btn = document.querySelector(trigger);
+
+//*Getting cards from layout
+/*      
     cards.forEach(card => {
         card.classList.add('animated', 'fadeInUp');
     });
@@ -13,6 +16,33 @@ const showMoreStyles = (trigger, style) => {
         });
         btn.remove();
     });
+*/
+
+    btn.addEventListener('click', function() {
+        getResource('assets/db.json')
+            .then(res => createCards(res.styles))
+            .catch(error => console.log(error));
+
+            this.remove();
+    });
+
+    function createCards(response) {
+        response.forEach(({src, title, link}) => {
+            let card = document.createElement('div');
+
+            card.classList.add('animated', 'fadeInUp', 'col-sm-3', 'col-sm-offset-0', 'col-xs-10', 'col-xs-offset-1');
+
+            card.innerHTML = `
+                <div class="styles-block">
+                    <img src=${src} alt="style">
+                    <h4>${title}</h4>
+                    <a href=${link}>Подробнее</a>
+                </div>
+            `;
+
+            document.querySelector(wrapper).appendChild(card);
+        });
+    }
 };
 
 export default showMoreStyles;
